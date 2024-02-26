7bd4289b698fdf8e24225b92fce3b5a35da76576
renato010200 <104111720+renato010200@users.noreply.github.com>
2024-02-26 20:06:05 -0300

    Add files via upload

---
+++ aula02/index.js
@@ -0,0 +1,60 @@
+console.log("Ola mundo!")
+
+texto = 100
+texto = "hello 2"
+console.log(texto)
+
+
+
+const texto2 = "texto fixo"
+
+console.log(texto2) 
+
+var variavelGlobal = "texto global"
+console.log(variavelGlobal)
+
+if(variavelGlobal == "outro texto global"){
+    console.log("Acertou!")
+} else{
+    console.log("Errou!")
+
+}
+
+
+if(texto = 0){
+ console.log("Verdade")
+}else{
+    console.log("Mentira")
+}
+
+for(let i = 0; i <10; i++){
+    console.log("Valor do i: ", i)
+}
+
+let numero1 = 5
+let numero2 = 10
+
+let soma = numero1 + numero2
+let subtracao = numero1 - numero2
+let mult = numero1 * numero2
+let div = numero1 / numero2
+console.log(soma,subtracao,mult,div)
+
+let numerotexto = "10"
+let num = 1
+
+let resultado = numerotexto + num
+console.log(resultado)
+
+let resultadoNum = parseInt(numerotexto) + num 
+console.log(resultadoNum)
+
+// criação de função
+function soma1(a,b){
+    return a + b
+}
+// declaração de função
+
+// expressão de função
+
+// arrow function
